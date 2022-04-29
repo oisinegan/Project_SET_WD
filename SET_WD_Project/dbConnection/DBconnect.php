773d8ce10d006b0e296d08272e3e03a2d9a7f49a
oisinegan <oisinegan00@gmail.com>
2022-04-29 12:13:25 +0100

    .

--- SET_WD_Project/dbConnection/DBconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../config.php'; //access the login values
-try {
-    $connection = new PDO($dsn, $username, $password, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-?>
